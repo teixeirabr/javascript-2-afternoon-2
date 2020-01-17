4b6a11de61d534a53f0ee544ffcb13f872f61855
andre <andrelaprov@gmail.com>
2020-01-16 17:47:55 -0700

    problem 2

--- practice.js
+++ practice.js
@@ -11,13 +11,18 @@
 // Do not edit the code below.
 var arr = [10,20,30];
 // Do not edit the code above.
-
+function first(arr) {
+  return arr[0]
+}
 /*
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
 
 //Code Here
+
+
+
 
 
 
@@ -34,6 +39,10 @@ var arr = [40,50,60];
 
 //Code Here
 
+function last(arr) {
+  return arr[2]
+}
+
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +57,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper (family) {
+  for (let i = 0; i < family.length; i++) {
+    alert(family[i])
+    
+  }
+  
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -64,6 +79,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+function reversedLooper(letters) {
+  for (let i = letters.length - 1; i>=0; i--) {
+    alert(letters[i]);
+    
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,8 +100,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
+function evenFinder (nums) {
+  for (let i = 0; i < nums.length; i++) {
+   if (nums[i] % 2 !== 0)
+   {
+     nums.splice(i,1);
+   } 
+    
+  }
+  return nums;
+}
 
 
 
